abe4cbe5585a78136081640fc10ce5d39ec26477
ezepdev <edjpinheiro@gmail.com>
2021-10-13 06:07:57 -0300

    cambio de logica del componente Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
 import './styles/Button.css'
 
 
-function Button({ children, variant,disabledShadow }) {
+function Button({ children, variant = "default", disabledShadow = false, color = 'color_default' }) {
+
+    const [styles,setStyles] = useState(
+        [
+            variant, color
+        ]
+    )
+
+    useEffect( {
+        if (disabledShadow) {
+            setStyles( styles => styles.concat('disabledShadow') )
+        }
+    },[disabledShadow])
 
     return (
-        <button className={`button ${variant} ${disabledShadow}`}>
-            { children }
+        <button className={`button ${styles}`}>
+            { children } 
         </button>
     )
 }

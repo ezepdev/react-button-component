d5725d9dd4ef58871b42f8ae1a333c1f8aa693b0
ezepdev <edjpinheiro@gmail.com>
2021-10-13 06:33:30 -0300

    Logica de componente Button mejorada

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,21 +4,25 @@ import './styles/Button.css'
 
 function Button({ children, variant = "default", disabledShadow = false, color = 'color_default' }) {
 
+    
     const [styles,setStyles] = useState(
         [
             variant, color
         ]
-    )
-
-    useEffect( {
+        )
+        
+    useEffect(() => {
         if (disabledShadow) {
             setStyles( styles => styles.concat('disabledShadow') )
+        }
+        else {
+            setStyles(styles => styles.filter(e => e !== 'disabledShadow'))
         }
     },[disabledShadow])
 
     return (
-        <button className={`button ${styles}`}>
-            { children } 
+        <button className={`button ${styles.join(' ')}`}>
+            { children }    
         </button>
     )
 }
